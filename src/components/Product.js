68c0e6452bff8c5f9dc94eec0783d8dbe06b20ba
Youpele Michael <mjayjesus@gmail.com>
2021-04-08 01:43:12 +0200

    formatPrice function added

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,7 +16,7 @@ const Product = ({ image, name, id, price }) => {
       </div>
       <footer>
         <h5>{name}</h5>
-        <p>${price}</p>
+        <p>{formatPrice(price)}</p>
       </footer>
     </Wrapper>
   )
